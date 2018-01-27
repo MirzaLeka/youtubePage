6fbd6e929574b7f53742aa31ce1b8db72132730e
SPY4ER <mirza_376@hotmail.com>
2018-01-27 15:45:11 +0100

    Added recent uploads slider

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -1,17 +1,58 @@
-$(document).ready(function(){
 
- $("select.moreOptions").change(function(){
-        var selectedChoice = $(".moreOptions option:selected").val();
+/* if you mouse over class Nonyx, activate nonyx and set name = nino, else activate spy4er and set name = mirza*/
+      function snackbarNonyx() {
+    var x = document.getElementById("snackbarNonyx")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
 
-        if (selectedChoice == 1) {
-            $("#hide").show();
-        }
-        else{
-            $("#hide").hide();
-        }
+      function snackbarSpy4er() {
+    var x = document.getElementById("snackbarSpy4er")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+var showText = function (target, message, index, interval) {   
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
+}
 
 
-    });
+$(function () {
 
+  var nxcTitle = "NEXT CREW";
 
+  showText("h1 b cite", nxcTitle, 0, 200);   
+
+});
+
+/* SLIDER */
+
+   var $item = $('.carousel .item'); 
+//var $wHeight = $(window).height(); == screen height
+$item.eq(0).addClass('active');
+$item.height("50vh"); 
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
 });
