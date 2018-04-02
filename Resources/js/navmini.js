36c859564d6828336f62ff1ab23bf634c9e9180e
Mirza Leka <mirza_376@hotmail.com>
2018-04-02 23:58:49 +0200

    Mini navbar prototype in work

---
+++ Resources/js/navmini.js
@@ -0,0 +1,27 @@
+
+var navActive = false;
+
+$("#openMiniNav").click(() => {
+
+    $(".outerNavUl").show();
+
+    if (navActive) {
+    
+  //      $(".outerNavUl").fadeOut(500);
+        $(".outerNavUl").animate({
+            top: -65
+        },1500);
+        
+        navActive = false;
+    }
+    else {
+//        $(".outerNavUl").fadeIn(500);
+        $(".outerNavUl").animate({
+            top: 45
+        },1500);
+        
+        
+        navActive = true;
+    }
+
+});
