e46c5bddfd4534a94e54cbcc2b3c63da551141bb
Mirza Leka <mirza_376@hotmail.com>
2018-04-03 00:32:01 +0200

    Animation is complete. Need to change colors and add JS

--- Resources/js/navmini.js
+++ Resources/js/navmini.js
@@ -2,26 +2,20 @@
 var navActive = false;
 
 $("#openMiniNav").click(() => {
-    $(".outerNavUl").show();
  
-
     if (navActive) {
       
-    
-  //      $(".outerNavUl").fadeOut(500);
         $(".outerNavUl").animate({
             top: -65
-        },1500);
-
-        
+        },800);
         
         navActive = false;
     }
     else {
-//        $(".outerNavUl").fadeIn(500);
+
         $(".outerNavUl").animate({
             top: 45
-        },1500);
+        },800);
         
         
         navActive = true;
