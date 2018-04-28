90a3ff0e5726e49c37bb09c2563fd94b7f6e7c96
Mirza Leka <mirza_376@hotmail.com>
2018-04-28 23:57:06 +0200

    Added overlay custom files

---
+++ Resources/js/overlay.js
@@ -0,0 +1,16 @@
+
+var videoTitles = [
+"Rainbow Six Siege | Battlefield Hardline Saga",    
+"Assassin's Creed: Family Bond Saga",
+"Comeback Series Saga",
+"One Piece: The Will of D Saga",
+"The Amazing Spiderman Saga"
+ ];
+
+$(".videoTitle").html(videoTitles[0]);
+
+// Use objects
+
+// Put two pics into object, object into array
+
+// use $ .html to cycle between objects ---> array[0].pic1, array[0].pic2
