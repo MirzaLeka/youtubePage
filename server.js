175a210e1b18cb085ca7d79a8d2bbf570bcf070e
Mirza Leka <mirza_376@hotmail.com>
2018-08-01 00:10:21 +0200

    Added error page when you mistype route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express'); 
 const app = express(); 
+const path = require('path');
+app.use(require('body-parser').urlencoded({extended: false}));
 
 const port = process.env.PORT || 3000; 
 
@@ -15,6 +17,29 @@ app.get("/", function(req, res)
 res.sendFile("index.html", {"root": __dirname + '/Resources/dist'}); 
 });
 
+app.get("/home", (req, res) => {
+    res.sendFile("index.html", {"root": __dirname + '/Web-Info'});
+    });
+
+/* Render Error Page */
+
+const router = express.Router();
+
+router.use(function(req, res, next) {
+    if (!req.route)
+        return next (new Error('404'));  
+    next();
+});
+
+router.use(function(err, req, res, next){
+    res.sendFile("error.html", { root: path.join(__dirname, '/Web-Info') });
+})
+
+router.use(function(req, res){
+    res.send(app.locals.test + '');
+});
+
+ app.use(router);
 
 app.listen(port, () => {
 console.log(`Server is running on port ${port}`);
